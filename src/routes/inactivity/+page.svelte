2f2a200a87e906a49818cf65121e7997df39f31c
Luis Yago Fernández <luisyagofdez@gmail.com>
2024-02-10 19:36:20 +0100

    FEAT(inactivity timeout): add inactivity timeout page

---
+++ src/routes/inactivity/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import InactivityCountdown from '@/routes/inactivity/components/InactivityCountdown.svelte'
+</script>
+
+<svelte:head>
+	<title>Inactivity</title>
+	<meta name="description" content="This countdown will restart every time a mouse event happens" />
+</svelte:head>
+
+<main>
+	<h1>Screen Inactivity</h1>
+	<InactivityCountdown />
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+	}
+</style>
---
+++ src/routes/inactivity/components/InactivityCountdown.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	// IMPORTS
+	import { onMount, onDestroy } from 'svelte'
+
+	// LIFECYCLE
+	onMount(() => {
+		beginListeningToScreenEvents()
+	})
+
+	onDestroy(() => {
+		clearTimeout(inactivityTimeout)
+	})
+
+	// DATA
+	let inactivityTimeout: ReturnType<typeof setTimeout> | undefined = undefined
+	let oneSecondTimeout: ReturnType<typeof setTimeout> | undefined = undefined
+	let secondsRemaining = 60
+
+	// FUNCTIONS
+	function beginListeningToScreenEvents() {
+		document.addEventListener('mousemove', resetInactivityTimeout)
+		document.addEventListener('click', resetInactivityTimeout)
+		resetInactivityTimeout()
+	}
+
+	function resetInactivityTimeout() {
+		resetCountdown()
+		startCountdown()
+		const oneMinInMs = 1 * 60 * 1000
+		clearTimeout(inactivityTimeout)
+		inactivityTimeout = setTimeout(() => {
+			window.alert('The app was inactive for one minute')
+		}, oneMinInMs)
+	}
+
+	function startCountdown() {
+		if (secondsRemaining > 0) {
+			clearTimeout(oneSecondTimeout)
+			oneSecondTimeout = setTimeout(() => {
+				secondsRemaining--
+				startCountdown()
+			}, 1000)
+		}
+	}
+
+	function resetCountdown() {
+		secondsRemaining = 60
+	}
+</script>
+
+<div class="countdown-wrapper">
+	<span>This countdown will restart every time a mouse event happens</span>
+	<span
+		>Countdown:
+		<span class="seconds">
+			{secondsRemaining}
+		</span>
+	</span>
+</div>
+
+<style>
+	.countdown-wrapper {
+		display: flex;
+		flex-direction: column;
+	}
+	span.seconds {
+		color: var(--color-theme-1);
+		font-size: 1.5rem;
+	}
+</style>
